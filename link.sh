f0410c31b2a25ce3b308f5e8103b79315931a967
fukuo <FukuoKadota@fukuo.local>
2014-11-27 02:12:47 +0900

    update dotfiles

--- link.sh
+++ link.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-# vim settings
-ln -s dots/vimrc ~/.vimrc
-
 # bash settings 
 ln -s dots/bashrc ~/.bashrc
 ln -s dots/bash_profile ~/.bash_profile
 
+# vim settings
+ln -s dots/vimrc ~/.vimrc
+ln -s vim-colors/moncolor.vim ~/.vim/colors/moncolor.vim
+
 # git settings
 ln -s dots/gitignore ~/.gitignore_global
+ln -s dots/gitconfig ~/.gitconfig
 
 # ag settings
 ln -s dots/agignore ~/.agignore
+
+# gem settings
+ln -s dots/gemrc ~/.gemrc
