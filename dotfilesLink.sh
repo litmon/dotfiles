92c7da75234ddf843cb7fba3d9a468d995dbc34c
fukuo <FukuoKadota@fukuo.local>
2014-11-27 02:00:25 +0900

    いろいろ追加

--- dotfilesLink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ln -s dots/.vimrc ~/.vimrc
-ln -s dots/.bashrc
-ln -s dots/.bash_profile ~/.bash_profile
-ln -s dots/.gitignore-global
-ln -s dots/.agignore
---
+++ link.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# vim settings
+ln -s dots/vimrc ~/.vimrc
+
+# bash settings 
+ln -s dots/bashrc ~/.bashrc
+ln -s dots/bash_profile ~/.bash_profile
+
+# git settings
+ln -s dots/gitignore ~/.gitignore_global
+
+# ag settings
+ln -s dots/agignore ~/.agignore
