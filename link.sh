29b5559783d7228c574d4c8ee23cca28ae5b5135
Fukuo Kadota <app.fk91@gmail.com>
2014-11-27 03:24:26 +0900

    update link.sh

--- link.sh
+++ link.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
+# ${0} の dirname を取得
+dotfiles_dir=`dirname "${0}"`
+
+# ${0} が 相対パスの場合は cd して pwd を取得
+expr "${0}" : "/.*" > /dev/null || dotfiles_dir=`(cd "${dotfiles_dir}" && pwd)`
+
 # bash settings 
-ln -s dots/bashrc ~/.bashrc
-ln -s dots/bash_profile ~/.bash_profile
+ln -s $dotfiles_dir/dots/bashrc ~/.bashrc
+ln -s $dotfiles_dir/dots/bash_profile ~/.bash_profile
 ## scripts
-ln -s git-scripts/git-completion.bash ~/.git-completion.bash
-ln -s git-scripts/git-prompt.sh ~/.git-prompt.sh
+ln -s $dotfiles_dir/git-scripts/git-completion.bash ~/.git-completion.bash
+ln -s $dotfiles_dir/git-scripts/git-prompt.sh ~/.git-prompt.sh
+
+## input settings
+ln -s $dotfiles_dir/dots/inputrc ~/.inputrc
 
 # vim settings
-ln -s dots/vimrc ~/.vimrc
-ln -s vim-colors/moncolor.vim ~/.vim/colors/moncolor.vim
+ln -s $dotfiles_dir/dots/vimrc ~/.vimrc
+ln -s $dotfiles_dir/vim-colors/moncolor.vim ~/.vim/colors/moncolor.vim
 
 # git settings
-ln -s dots/gitignore ~/.gitignore_global
-ln -s dots/gitconfig ~/.gitconfig
+ln -s $dotfiles_dir/dots/gitignore ~/.gitignore_global
+ln -s $dotfiles_dir/dots/gitconfig ~/.gitconfig
 
 # ag settings
-ln -s dots/agignore ~/.agignore
+ln -s $dotfiles_dir/dots/agignore ~/.agignore
 
 # gem settings
-ln -s dots/gemrc ~/.gemrc
+ln -s $dotfiles_dir/dots/gemrc ~/.gemrc
