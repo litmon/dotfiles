dd8eba6d46aacfb33d2d6de4142b8f129d7d9985
Fukuo Kadota <app.fk91@gmail.com>
2014-11-29 15:38:02 +0900

    update vimrc

--- link.sh
+++ link.sh
@@ -8,26 +8,29 @@ expr "${0}" : "/.*" > /dev/null || dotfiles_dir=`(cd "${dotfiles_dir}" && pwd)`
 
 ### TODO dots/以下のファイルをすべてホームディレクトリにシンボリックリンクを貼る
 # bash settings 
-ln -s $dotfiles_dir/dots/bashrc ~/.bashrc
-ln -s $dotfiles_dir/dots/bash_profile ~/.bash_profile
+ln -s ${dotfiles_dir}/dots/bashrc ~/.bashrc
+ln -s ${dotfiles_dir}/dots/bash_profile ~/.bash_profile
 ## scripts
-ln -s $dotfiles_dir/git-scripts/git-completion.bash ~/.git-completion.bash
-ln -s $dotfiles_dir/git-scripts/git-prompt.sh ~/.git-prompt.sh
+ln -s ${dotfiles_dir}/git-scripts/git-completion.bash ~/.git-completion.bash
+ln -s ${dotfiles_dir}/git-scripts/git-prompt.sh ~/.git-prompt.sh
 
 ## input settings
-ln -s $dotfiles_dir/dots/inputrc ~/.inputrc
+ln -s ${dotfiles_dir}/dots/inputrc ~/.inputrc
 
 # vim settings
-ln -s $dotfiles_dir/dots/vimrc ~/.vimrc
-ln -s $dotfiles_dir/vim-colors/moncolor.vim ~/.vim/colors/moncolor.vim
+if [ -d $HOME/.vim/colors ]; then
+	mkdir -p $HOME/.vim/colors
+fi
+ln -s ${dotfiles_dir}/dots/vimrc ~/.vimrc
+ln -s ${dotfiles_dir}/vim-colors/moncolor.vim ~/.vim/colors/moncolor.vim
 
 # git settings
-ln -s $dotfiles_dir/dots/gitignore ~/.gitignore_global
-ln -s $dotfiles_dir/dots/gitconfig ~/.gitconfig
-ln -s $dotfiles_dir/git-scripts/git_diff_wrapper /usr/local/bin/git_diff_wrapper
+ln -s ${dotfiles_dir}/dots/gitignore ~/.gitignore_global
+ln -s ${dotfiles_dir}/dots/gitconfig ~/.gitconfig
+ln -s ${dotfiles_dir}/git-scripts/git_diff_wrapper /usr/local/bin/git_diff_wrapper
 
 # ag settings
-ln -s $dotfiles_dir/dots/agignore ~/.agignore
+ln -s ${dotfiles_dir}/dots/agignore ~/.agignore
 
 # gem settings
-ln -s $dotfiles_dir/dots/gemrc ~/.gemrc
+ln -s ${dotfiles_dir}/dots/gemrc ~/.gemrc
