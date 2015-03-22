957bfa794bb1f595fad30047c99cce8e23d62357
Fukuo Kadota <app.fk91@gmail.com>
2015-03-23 05:05:41 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,18 @@
-#!/usr/bin/sh
+#!/bin/sh
+
+echo "===== brew installing ====="
+if [ `which brew` ]; then
+  echo "brew is already installed.\n"
+else
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+echo "===== ansible installing ====="
+if [ `which ansible` ]; then
+  echo "ansible is already installed.\n"
+else
+  brew install ansible
+fi
+
+echo "===== setup from ansible playbook. ====="
+ansible-playbook ansible/localhost.yml
