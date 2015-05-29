3f9be760b1a8070b862002332667f272ed0f5769
Fukuo Kadota <app.fk91@gmail.com>
2015-05-30 01:33:42 +0900

    Write serverspec

--- kitchen/recipe.rb
+++ kitchen/recipe.rb
@@ -1,10 +1,11 @@
 package "epel-release"
+package "nginx"
 
 execute "Add repository remi to rpm" do
   command "rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm"
+  not_if "rpm -q remi-release-7.0-1.el7.remi.noarch"
 end
 
-package "nginx"
 package "php55" do
   action :install
   options "--enablerepo=remi --enablerepo=remi-php55"
@@ -15,7 +16,7 @@ service "nginx" do
 end
 
 template "/etc/nginx/nginx.conf" do
-  source "templates/nginx.conf"
+  source "templates/nginx.conf.erb"
   variables(root: "/var/www/")
   notifies :reload, "service[nginx]"
 end
